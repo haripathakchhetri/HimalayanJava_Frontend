e41542fc9b10aeaeae75afc9599d9451422c9450
haripathakchhetri <pathakhari570@gmail.com>
2024-10-25 18:06:14 +0545

    ch

--- src/app/constants/spi_urls.js
+++ src/app/constants/spi_urls.js
@@ -1,3 +1,3 @@
-export const baseUrl = "http://192.168.10.88:3000/api"
+export const baseUrl = "https://himalayan-java-clone-backend.onrender.com/api"
 
-export const imageUrl = "http://192.168.10.88:3000";
+export const imageUrl = "https://himalayan-java-clone-backend.onrender.com";
